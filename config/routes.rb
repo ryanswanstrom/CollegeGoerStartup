e73e86d9bff468c4bb816463cb97dfa733b9e36a
virtualrails <virtualrails@jay.linux>
2010-09-30 19:57:19 +0200

    adding the about page

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
 
   map.connect '', :controller => "application"
+  map.connect 'about', :controller => "application", :action => 'about'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
